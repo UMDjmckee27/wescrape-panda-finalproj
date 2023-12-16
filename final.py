01b1d0066e950a4f84d4fbe4fb0f86d525872b39
skidmarkjack <mckeejack279@gmail.com>
2023-12-16 17:45:26 -0500

    random comments and bs4

--- final.py
+++ final.py
@@ -3,4 +3,16 @@ import requests
 import pandas as pd
 import numpy as np
 
+#url being scraped 
 wikipedia_page = "https://en.wikipedia.org/wiki/The_World%27s_Billionaires"
+
+#sends a request to server and gets a response
+getting_request = requests.get(wikipedia_page)
+
+#uses beautifulsoup to parse through the web page and gather data
+wiki_parse = BeautifulSoup(getting_request.content, "html")
+
+
+
+
+
